2ca29858be1914ed681081d06568437ac903135b
claytran <56480415+claytran@users.noreply.github.com>
2024-04-01 13:22:01 -0400

    move examples to Aurora

--- powershell/example_code/aurora/list_engine_versions.ps1
+++ powershell/example_code/aurora/list_engine_versions.ps1
@@ -1,7 +1,7 @@
-# snippet-start:[powershell.rds.list_engine_versions]
+# snippet-start:[powershell.aurora.list_engine_versions]
 Get-RDSOrderableDBInstanceOption `
   -Engine aurora-postgresql `
   -DBInstanceClass db.r5.large `
   -Region us-east-1  | `
 Format-Table EngineVersion, SupportedEngineModes
-# snippet-end:[powershell.rds.list_engine_versions]
+# snippet-end:[powershell.aurora.list_engine_versions]
--- powershell/example_code/aurora/list_instance_classes.ps1
+++ powershell/example_code/aurora/list_instance_classes.ps1
@@ -1,7 +1,7 @@
-# snippet-start:[powershell.rds.list_instance_classes]
+# snippet-start:[powershell.aurora.list_instance_classes]
 Get-RDSOrderableDBInstanceOption `
   -Engine aurora-postgresql `
   -EngineVersion 13.6 `
   -Region us-east-1  | `
 Format-Table DBInstanceClass, SupportedEngineModes
-# snippet-end:[powershell.rds.list_instance_classes]
+# snippet-end:[powershell.aurora.list_instance_classes]

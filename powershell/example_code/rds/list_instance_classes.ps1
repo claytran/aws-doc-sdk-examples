2446b4e978653dde4a61b4da3e5eed3c3dd75be2
claytran <56480415+claytran@users.noreply.github.com>
2024-04-01 12:57:33 -0400

    remove comments that duplicate the descriptions

--- powershell/example_code/rds/list_instance_classes.ps1
+++ powershell/example_code/rds/list_instance_classes.ps1
@@ -1,5 +1,4 @@
 # snippet-start:[powershell.rds.list_instance_classes]
-# This example lists the DB instance classes that are supported for a specific DB engine version in an &AWS-Region;.
 Get-RDSOrderableDBInstanceOption `
   -Engine aurora-postgresql `
   -EngineVersion 13.6 `

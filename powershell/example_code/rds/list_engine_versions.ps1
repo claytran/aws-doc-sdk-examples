e25bd56411990e8e0c5dd7168354bed06e262422
claytran <56480415+claytran@users.noreply.github.com>
2024-04-01 12:17:11 -0400

    fix &AWS; entities

--- powershell/example_code/rds/list_engine_versions.ps1
+++ powershell/example_code/rds/list_engine_versions.ps1
@@ -1,5 +1,5 @@
 # snippet-start:[powershell.rds.list_engine_versions]
-# This example lists the DB engine versions that support a specific DB instance class in an AWS Region.
+# This example lists the DB engine versions that support a specific DB instance class in an &AWS-Region;.
 Get-RDSOrderableDBInstanceOption `
   -Engine aurora-postgresql `
   -DBInstanceClass db.r5.large `
